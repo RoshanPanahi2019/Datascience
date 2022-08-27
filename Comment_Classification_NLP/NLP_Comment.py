142c9eabd7a2b69d3682a6f73df225df81943da6
Ryan <roshanpanahico@gmail.com>
2022-08-27 12:36:10 -0700

    - NLP_Comment: Grid Search added for Random Forest. Acc: %90

--- Comment_Classification_NLP/NLP_Comment.py
+++ Comment_Classification_NLP/NLP_Comment.py
@@ -6,19 +6,21 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import RandomizedSearchCV# Number of trees in random forest
+from pprint import pprint
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
-import matplotlib.pyplot as plt
 from pickle import TRUE
-from pydoc import doc
 from tkinter.tix import COLUMN
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import re
 import nltk
-from nltk.stem import WordNetLemmatizer
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
@@ -29,10 +31,9 @@ def my_read_file(path):
     data=data.rename(columns={"Enter Comments ":"Comment","Root_Cause_for_Slip_or_Push":"Label"}) # rename columns.
     return data
 
-documents=[]
-stemmer = WordNetLemmatizer()
-
 def pre_process(data):
+    documents=[]
+    stemmer = WordNetLemmatizer()
     for doc in range(len(data)):
         document = re.sub(r'\W', ' ', str(data["Comment"][doc])) # Remove all the special characters
         document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document) # remove all single characters
@@ -46,23 +47,40 @@ def pre_process(data):
         documents.append(document)
     return documents
 
-# Tokenizing & Vectorizing
-def vectorize(documents,y):
+def vectorize(documents,y): # Tokenizing & Vectorizing
     vectorizer=CountVectorizer() # Create the vectorizer object. 
     X=vectorizer.fit_transform(documents).toarray() # Tokinize & count number of occurances.
     tfidfconverter = TfidfTransformer() # term frequency–inverse document frequency
     X = tfidfconverter.fit_transform(X).toarray()
     return X
 
-def train_test(X,y):
+def train(X,y,param_grid):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=100) # Split to train & test. 
-    classifier = RandomForestClassifier(n_estimators=1000, random_state=100)
-    classifier.fit(X_train, y_train)
-    y_pred = classifier.predict(X_test)
-    print(confusion_matrix(y_test,y_pred))
-    print(classification_report(y_test,y_pred))
-    print(accuracy_score(y_test, y_pred))
-    return(y_test, y_pred)
+    rf = RandomForestClassifier(n_estimators=1000, random_state=100)
+    rf_random = RandomizedSearchCV(estimator = rf, param_distributions = param_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+    rf_random.fit(X_train, y_train)
+    print(rf_random.best_params_)
+    return(X_test,y_test,rf_random.best_estimator_)
+
+def evaluate(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    return predictions
+
+def grid_search():
+    n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)] # Number of features to consider at every split
+    max_features = ['auto', 'sqrt'] # Maximum number of levels in tree
+    max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+    max_depth.append(None)
+    min_samples_split = [2, 5, 10]     # Minimum number of samples required to split a node
+    min_samples_leaf = [1, 2, 4]     # Minimum number of samples required at each leaf node
+    bootstrap = [True, False] # Method of selecting samples for training each tree
+    random_grid = {'n_estimators': n_estimators, # Create the random grid
+                'max_features': max_features,
+                'max_depth': max_depth,
+                'min_samples_split': min_samples_split,
+                'min_samples_leaf': min_samples_leaf,
+                'bootstrap': bootstrap}
+    return(random_grid)
 
 def cal_accuracy(y_test, y_pred):
         
@@ -73,7 +91,7 @@ def cal_accuracy(y_test, y_pred):
     plt.title("Classifying Field Crew Comments to 25 Delay Root Cause Classes")
     plt.xlabel("Predicted Labels")
     plt.ylabel("True Labels")
-    plt.show()
+    #plt.show()
 
 # move this to the schedule classifer script. 
 #def Visualize(rf):
@@ -92,12 +110,12 @@ def cal_accuracy(y_test, y_pred):
 if __name__=="__main__":
     path="/media/ms/D/myGithub_Classified/Skanska/NLP/Comment_Root_Cause_EntireData_08262022.csv"
     data=my_read_file(path)
-    dist_label=data['Label'].value_counts() # Statistics of the dataset.
-    data["Label"].hist(bins=100)
-    plt.show()
-    exit()
-    
+    #dist_label=data['Label'].value_counts() # Statistics of the dataset.
+    #data["Label"].hist(bins=100)
+    #plt.show()
     documents=pre_process(data)
     vector=vectorize(documents,data["Label"])
-    y_test, y_pred=train_test(vector,data["Label"])
+    param_grid=grid_search()
+    X_test,y_test,rf_model_best=train(vector,data["Label"],param_grid)
+    y_pred = evaluate(rf_model_best, X_test, y_test)
     cal_accuracy(y_test, y_pred)
