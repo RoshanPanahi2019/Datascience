dfd2fcc3f9702ec36ed6855e069d85186fa70516
Ryan <roshanpanahico@gmail.com>
2022-10-27 13:32:29 -0700

    basic OCR.

--- RFI_Project/ocr.py
+++ RFI_Project/ocr.py
@@ -1,11 +1,14 @@
 import cv2
 import pytesseract
 from pytesseract import Output
-root="/media/mst/Backup/dataset/OCR"
-img = cv2.imread(root+'/example2.png')
+import pandas as pd
+root="/media/ms/D/myGithub_Classified/Skanska/Data"
+dst="/media/ms/D/myGithub_Classified/Skanska/Data"
+img = cv2.imread(root+'/floorplan1.png')
+df=pd.DataFrame
+df = pytesseract.image_to_data(img, output_type=Output.DICT)
+print(df["text"])
 
-data = pytesseract.image_to_data(img, output_type=Output.DICT)
-print(data["text"])
 # extract words from a drawing set
 # check the accuracy 
 # match with the tagged RFIs
